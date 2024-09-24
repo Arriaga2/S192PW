67bcb8bc6a5c4ffacbe4c4e8de4784fb8e605b74
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2024-09-24 17:42:14 -0600

    add introLaravel

---
+++ introLaravel/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    
+    @vite (['resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+    <h1> Hola mundo - Intro Laravel</h1>
+</body>
+</html>
