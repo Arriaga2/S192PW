f4f9630eeeb6379b60c2286f7d1455f7e7b0b37c
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2024-10-09 22:57:23 -0600

    PRACTICA 4

--- introLaravel/routes/web.php
+++ introLaravel/routes/web.php
@@ -1,6 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+Route::get('/',[controladorVistas::class, 'home'])->name('rutainicio');
+
+Route::get('/form', [controladorVistas::class, 'formulario'])->name('rutaform');
+
+Route::get('/clientes', [controladorVistas::class, 'consulta'])->name('rutaclientes');
+
+
+Route::view('/component', 'componentes')->name('rutacomponentes');
+
+
+
+
 
 //ruta tipo get
 /* Route::get('/', function () {
@@ -8,10 +22,11 @@ use Illuminate\Support\Facades\Route;
 }); */
 
 //ruta tipo view
-Route::view('inicio/', 'inicio')->name('rutainicio');
+/* Route::view('inicio/', 'inicio')->name('rutainicio');
 
 Route::view('/form', 'formulario')->name('rutaform');
 
 Route::view('/client', 'clientes')->name('rutaclientes');
 
-Route::view('/component', 'componentes')->name('rutacomponentes');
+Route::view('/component', 'componentes')->name('rutacomponentes'); */
+
