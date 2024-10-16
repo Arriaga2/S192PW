b6d41b29d2f62d151df814d2f3cd76740e3b57b6
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2024-10-16 12:28:47 -0600

    3 y 4
    Se ha agregado la portada con mis datos personales y se ha agregado el boton llamado REPASO1

---
+++ prácticasRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/js/app.js']) 
+    <title>Repaso</title>
+</head>
+<body>
+<div class="card text-center">
+  <div class="card-header">
+    <ul class="nav nav-pills card-header-pills">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Página 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" aria-disabled="true">Página 2</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Repaso 1</h5>
+    <p class="card-text">    
+    <b>Nombre:</b> GAEL ARRIAGA FELIPE<br>
+    <b>Matrícula:</b> 122043647<br>
+    <b>Carrera:</b> SISTEMAS<br>
+    <b>Generación:</b> 19<br>
+    <b>Grupo:</b> S192<br>
+    <b>Último Cuatrimestre:</b> 7<br>
+    </p>
+    <a href="#" class="btn btn-primary">REPASO 1 </a>
+  </div>
+</div>
+
+</body>
+</html>
+
