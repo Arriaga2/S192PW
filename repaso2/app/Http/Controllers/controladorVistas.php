26ca7e9e5ce61facc5516c93ba5dee66959e2074
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2024-10-31 01:17:04 -0600

    controlador

--- repaso2/app/Http/Controllers/controladorVistas.php
+++ repaso2/app/Http/Controllers/controladorVistas.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\validadorLibros;
 use Illuminate\Http\Request;
 
 class controladorVistas extends Controller
@@ -9,7 +10,16 @@ class controladorVistas extends Controller
     public function inicio(){
         return view('inicio');
     }
+
     public function registro(){
         return view('registro');
+    }
+
+    public function procesarLibro(validadorLibros $request){
+        $titulo = $request->input('titulo');
+
+        session()->flash('exito', 'El libro "' . $titulo . '" ha sido registrado exitosamente.');
+
+        return redirect()->route('rutaregistro'); 
     }
 }
